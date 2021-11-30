93b3c06cab190bb9613b5d8d88cb6c7b4d0cc843
liuxingbaoma <3568721910@qq.com>
2021-11-30 22:20:58 +0800

    wqe

--- autotest/src/test/java/api_suite/Api2_Test.java
+++ autotest/src/test/java/api_suite/Api2_Test.java
@@ -3,10 +3,6 @@ package api_suite;
 public class Api2_Test {
 	public static void main(String[] args) {
 		System.out.println("hellowordasdasdasd");
-		System.out.println("hellowordasdasdasd");
-		System.out.println("hellowordasdasdasd");
-		System.out.println("hellowordasdasdasd");
-		System.out.println("hellowordasdasdasd");
-		System.out.println("hellowordasdasdasd");
+
 	}
 }
